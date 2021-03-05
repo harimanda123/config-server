6dbf39facfa9b9ce819dad1e4239736ea9d35a39
Vamsi Krishna Aitha <ivamsi2001@gmail.com>
2021-03-05 22:57:14 +0530

    Initial commit

---
+++ src/test/java/com/techhmr/configserver/TechhmrConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techhmr.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechhmrConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
